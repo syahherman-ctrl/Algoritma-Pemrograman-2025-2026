547cdf33aaec50deba628f83aad8f6498e7eb70a
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-13 09:51:20 +0700

    Latihan c atau c++

---
+++ c atau c++/coba1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+main()
+{
+    int a = 7;
+    int b = 2;
+    char c[15] = "Muhammad Encep";
+
+    puts("Nama");
+    puts(c);
+    printf("Terdiri dari %d Huruf dan %d Kata",a,b);
+    return 0;
+}
