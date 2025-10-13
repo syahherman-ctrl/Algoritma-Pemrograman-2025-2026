c3a48ea01c06cc4074586333f1d3d101ff815a98
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-13 11:19:20 +0700

    tugas1 c++

---
+++ c atau c++/tugas1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    string nama = "Hermansyah";
+    int umur = 22;
+    float tinggi = 175.5;
+    bool aktif = true;
+
+    cout<<"Nama = "<<nama <<endl;
+    cout<<"umur = "<<umur <<endl;
+    cout<<"Tinggi = "<<tinggi<<endl;
+    cout<<"Status Aktif = "<<aktif<<endl;
+
+}
+
