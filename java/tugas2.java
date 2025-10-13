799aa7af84302448f0b36966d0e7dd35928c139a
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-13 12:06:44 +0700

    tugas2 java

---
+++ java/tugas2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukan Jari-Jari = ");
+
+        double r =sc.nextDouble();
+        double luas = PI *r*r;
+
+        System.out.println("Luas Lingkaran = "+ luas);
+    }
+    
+}
