f57d91300c511beb4301277a2fd02ff65d6353a7
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-13 11:25:36 +0700

    tugas1 java

---
+++ java/tugas1.java
@@ -0,0 +1,16 @@
+class tugas1{
+    public static void main(){
+        String nama = "Hermansyah";
+        int umur = 22;
+        double tinggi = 175.5;
+        boolean aktif = true;
+
+        System.out.println("Nama = "+ nama);
+        System.out.println("Umur = " + umur);
+        System.out.println("Tinggi = "+ tinggi);
+        System.out.println("Status aktif = "+ aktif);
+
+
+    }
+
+}
