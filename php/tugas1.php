a09ff1db40446ade03b338fd3aa32309aaae9982
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-13 11:45:25 +0700

    tugas1 php

---
+++ php/tugas1.php
@@ -0,0 +1,11 @@
+<?php
+    $nama = "Hermansyah";
+    $umur = 22;
+    $tinggi = 175.5;
+    $aktif = true;
+
+    echo "Nama = $nama \n";
+    echo "umur = $umur \n";
+    echo "tinggi = $tinggi \n";
+    echo "Status Aktif = $aktif \n";
+?>
