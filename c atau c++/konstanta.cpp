b7aee124a156c67c7fc812e95dea59765fbd6c2a
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-20 11:07:54 +0700

    Konstanta c++

---
+++ c atau c++/konstanta.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+main(){
+    const double PI = 3.14;
+    const int Nilai_max = 100;
+    const char MyChar = 'A';
+    const string MyName = "Muhammad Encep";
+
+    cout<<"Belajar konstanta"<<endl;
+    cout<<"Konstanta PI"<<PI<<endl;
+    cout<<"Konstanta Nilai_Max : "<<Nilai_max<<endl;
+    cout<<"Konstanta MyChar : "<<MyChar<<endl;
+    cout<<"Konstanta MyName : "<<MyName<<endl;
+    cout<<endl;
+}
