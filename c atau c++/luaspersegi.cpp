08641381ec5df31a3ed7884fc1ff0717021be917
syahherman-ctrl <syahhermansy@gmail.com>
2025-10-27 09:51:48 +0700

    Luas Persegi

---
+++ c atau c++/luaspersegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   int s1 = 2;
+   int s2 = 4;
+   int LPersegi = s1 * s2;
+
+   cout << "Luas Persegi : "<< LPersegi ;
+}
